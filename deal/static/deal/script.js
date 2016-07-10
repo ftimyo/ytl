9db07bde37b817e5e715caf82838f512e07252c2
tim <tim@Narwhal.stny.rr.com>
2016-07-10 03:00:03 -0400

    add plus minus function in cart and item count, need backend support

--- deal/static/deal/script.js
+++ deal/static/deal/script.js
@@ -267,6 +267,7 @@ function LoadGallery(server, dishid) {
 	xhttp.open("GET", server+'?mealid='+dishid, true);
   xhttp.send();
 }
+/*{key:[name,price,cnt],}*/
 function UpdateCartCount() {
 	var cart = GetCookie("cart");
 	var ncart = document.getElementById("ncart");
@@ -275,66 +276,105 @@ function UpdateCartCount() {
 		ncart.innerHTML = 0;
 	} else {
 		cart = JSON.parse(cart);
-		ncart.innerHTML = (cart.length/3);
+		var cnt = 0;
+		for (var v in cart) {
+			cnt+=cart[v][2];
+		}
+		ncart.innerHTML = cnt;
 	}
 }
 function TotalPayment(cart) {
 	var total = 0.00;
-	for (var i = 2; i < cart.length; i+=3) {
-		total += parseInt(cart[i]);
+	for (var k in cart) {
+		total += cart[k][2] * cart[k][1];
 	}
 	document.getElementById("total").innerHTML = "CDN$ " + total.toFixed(2);
 }
 function AddItemFromMenu() {
-	var price = this.parentNode.getElementsByClassName("price")[0].innerHTML;
+	var price = parseFloat(this.parentNode.getElementsByClassName("price")[0].innerHTML);
 	var itemid = this.parentNode.getElementsByClassName("itemid")[0].innerHTML;
 	var mname = this.parentNode.getElementsByClassName("mname")[0].innerHTML;
 	var cart = GetCookie("cart");
 	if (cart == "") {
-		cart = [mname,itemid,price];
+		cart = {};
+		cart[itemid] = [mname,price,1];
 	} else {
-		cart = JSON.parse(cart);		
-		cart = cart.concat([mname,itemid,price]);
+		cart = JSON.parse(cart);
+		if (cart[itemid]) {
+			cart[itemid][2] += 1;
+		} else {
+			cart[itemid] = [mname,price,1];
+		}
 	}
 	SetCookie("cart", JSON.stringify(cart),24);
 	UpdateCartCount();
+	$("#cartview").modal();
+	ShowCartContent();
 }
 function AddItemFromDetail() {
-	var price = document.getElementById("price").innerHTML;
+	var price = parseFloat(document.getElementById("price").innerHTML);
 	var itemid = document.getElementById("itemid").innerHTML;
 	var mname = document.getElementById("mname").innerHTML;
 	var cart = GetCookie("cart");
 	if (cart == "") {
-		cart = [mname,itemid,price];
+		cart = {};
+		cart[itemid] = [mname,price,1];
 	} else {
-		cart = JSON.parse(cart);		
-		cart = cart.concat([mname,itemid,price]);
+		cart = JSON.parse(cart);
+		if (cart[itemid]) {
+			cart[itemid][2] += 1;
+		} else {
+			cart[itemid] = [mname,price,1];
+		}
 	}
 	SetCookie("cart", JSON.stringify(cart),24);
 	UpdateCartCount();
+	$("#cartview").modal();
+	ShowCartContent();
 }
 
 function EmptyCart() {
 	SetCookie("cart","",24);
 	document.getElementById("cartt").innerHTML = '<p style="font-size:20px;" class="w3-center">訂餐車已清空</p>';
-	TotalPayment([]);
+	TotalPayment({});
 	UpdateCartCount();
 }
 
 function RemoveItem(obj) {
-	var idx = parseInt(obj.name);
+	var key = obj.name;
 	var items = JSON.parse(GetCookie("cart"));
-	console.log(items);
-	items.splice(idx,3);
-	console.log(items);
-	if (items.length != 0) {
+	delete items[key];
+	var cnt = 0; for (var v in items){++cnt;}
+	if (cnt != 0) {
 		SetCookie("cart",JSON.stringify(items),24);
 	} else {
 		SetCookie("cart","",24);
 	}
 	ShowCartContent();
-	UpdateCartCount();
-	TotalPayment(items);
+}
+
+function PlusItem(obj) {
+	var cntf = obj.getElementsByClassName('cnt')[0];
+	var cnt = parseInt(cntf.innerHTML);
+	++cnt;
+	console.log(cnt);
+	cntf.innerHTML = cnt;
+	var items = JSON.parse(GetCookie('cart'));
+	items[obj.name][2] = cnt;
+	SetCookie('cart',JSON.stringify(items),24);
+	ShowCartContent();
+}
+function MinusItem(obj) {
+	var cntf = obj.getElementsByClassName('cnt')[0];
+	var cnt = parseInt(cntf.innerHTML);
+	if (cnt <= 1) {ShowCartContent();return;}
+	--cnt;
+	console.log(cnt);
+	cntf.innerHTML = cnt;
+	var items = JSON.parse(GetCookie('cart'));
+	items[obj.name][2] = cnt;
+	SetCookie('cart',JSON.stringify(items),24);
+	ShowCartContent();
 }
 
 function ShowCartContent() {
@@ -342,7 +382,8 @@ function ShowCartContent() {
 	var items = GetCookie("cart");
 	if (items == "") {
 		cartt.innerHTML = '<p style="font-size:20px;" class="w3-center">您的訂餐車沒有餐品</p>';
-		TotalPayment([]);
+		UpdateCartCount();
+		TotalPayment({});
 		return;
 	}
 	items = JSON.parse(items);
@@ -350,23 +391,27 @@ function ShowCartContent() {
 	var tbl = document.createElement("table");
 	tbl.className = "w3-table";
 	var tbh = document.createElement("thead");
-	tbh.innerHTML = "<tr><th>餐品名稱</th><th>價格</th></tr>"
+	tbh.innerHTML = "<tr><th>餐品名稱</th><th class='w3-padding'>數量</th><th>價格</th><th class='w3-right'>移除</th></tr>";
+	tbl.appendChild(tbh);
 	var tblb = document.createElement("tbody");
 	tblb.style = "font-size:20px;"
-	//tbl.appendChild(tbh);
 	tbl.appendChild(tblb);
 	cartt.appendChild(tbl);
-	for (var i = 0; i < items.length; i+=3) {
+	for (var k in items) {
 		var row = document.createElement("tr");
-		row.innerHTML = "<td>"+items[i]+"</td>"+"<td>CDN$ "+items[i+2]+"</td>";
+		row.name = k;
+		row.innerHTML = "<td>"+items[k][0]+
+			"</td><td><a class='mc'><span class='fa fa-minus-circle'/></a><span class='w3-padding cnt'>"+
+			items[k][2]+"</span><a class='pc'><span class='fa fa-plus-circle'/></a></td><td>CDN$ "+
+			(items[k][1]*items[k][2]).toFixed(2)+"</td>";
+		row.getElementsByClassName("pc")[0].addEventListener('click',(function(td){return function(){PlusItem(td);}})(row),false);
+		row.getElementsByClassName("mc")[0].addEventListener('click',(function(td){return function(){MinusItem(td);}})(row),false);
 		var rmbtn = document.createElement("td");
 		rmbtn.innerHTML = '<span style="cursor:pointer;" class="fa fa-times w3-closebtn"></span>';
-		rmbtn.name = i;
-		rmbtn.onclick = (function(td){return function(){RemoveItem(td);}})(rmbtn);
+		rmbtn.onclick = (function(td){return function(){RemoveItem(td);}})(row);
 		row.appendChild(rmbtn);
 		tblb.appendChild(row);
 	}
 	UpdateCartCount();
 	TotalPayment(items);
 }
-
